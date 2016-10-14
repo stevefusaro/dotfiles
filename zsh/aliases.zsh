87d7dc097705195bdba0e7a34f6aaf4a773dbda1
Steve Fusaro <sfusaro1@gmail.com>
2016-10-13 20:07:32 -0400

    Added sublime settings

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,6 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+# I instead handle this as a /bin script
+# alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
