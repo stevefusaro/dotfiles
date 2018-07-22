c652d77e09da9dd63851e3ea45cadbe72558a974
Steven Fusaro <steve@philosophie.is>
2018-07-22 18:41:57 -0400

    Changes from first philosophie computer

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,4 +3,4 @@ alias reload!='. ~/.zshrc'
 alias cls='clear' # Good 'ol Clear Screen command
 
 # I instead handle this as a /bin script
-# alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
+alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
